024e262ca3a2cf2ca04c17d289ab3165b27620b2
Pablo Cardozo <cardozopabloj@gmail.com>
2023-05-22 09:11:55 -0300

    0402

--- 04.c
+++ 04.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int multiplicacion(int a, int b) {
+// ############################# EJ 01 #######################################################################################
+
+// int multiplicacion(int a, int b) {
+//     int num = 0;
+//     if ( b==0 ) {
+//         num = 0;
+//     } else if ( b>0 ) {
+//         // printf("%d, %d\n", a, b);
+//         num = a + multiplicacion(a,b-1);  
+//     } else if ( b<0 ) {
+//         // printf("%d, %d\n", a, b);
+//         num = a + multiplicacion(a,b+1);  
+//     } 
+//     return num;
+// }
+
+// int main() {
+//     int num = multiplicacion(2, 3);
+//     printf("%d", num);
+
+//     return 0;
+// }
+
+// ############################# EJ 02 #######################################################################################
+
+int cociente(int a, int b) {
     int num = 0;
-    if ( b==0 ) {
+    if (a >= b) {
+        num = 1 + cociente(a-b, b);
+    } else {
         num = 0;
-    } else if ( b>0 ) {
-        // printf("%d, %d\n", a, b);
-        num = a + multiplicacion(a,b-1);  
-    } else if ( b<0 ) {
-        // printf("%d, %d\n", a, b);
-        num = a + multiplicacion(a,b+1);  
-    } 
+    }
     return num;
 }
 
 int main() {
-    int num = multiplicacion(2, 3);
+    int num = cociente(9, 3);
     printf("%d", num);
 
     return 0;
