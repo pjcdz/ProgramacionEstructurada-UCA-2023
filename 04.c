c1cf611f1269fd70131d533ff634175c836c8b3b
Pablo Cardozo <cardozopabloj@gmail.com>
2023-05-22 15:25:49 -0300

    0404

--- 04.c
+++ 04.c
@@ -45,46 +45,84 @@
 
 // ############################# EJ 03 #######################################################################################
 
-int resto(int a, int b) {
-    int num = 0;
-    if ( a>=b ) {
-        num = resto(a-b, b);
-    } else {
-        num = a;
-    } return num;
+// int resto(int a, int b) {
+//     int num = 0;
+//     if ( a>=b ) {
+//         num = resto(a-b, b);
+//     } else {
+//         num = a;
+//     } return num;
+// }
+
+// // int resto(10, 3) {
+// //     if (10>3) {
+// //         return 1 + resto(7, 3);
+// //     }
+// // }
+
+// // int resto(7, 3) {
+// //     if (7>3) {
+// //         return 1 + resto(4, 3);
+// //     }
+// // }
+
+// // int resto(4, 3) {
+// //     if (4>3) {
+// //         return 1 + resto(1, 3);
+// //     }
+// // }
+
+// // num = 3
+
+// // int resto(1, 3) {
+// //     if (1>3) {
+// //         no
+// //     } else {
+// //         num = 1
+// //     }
+// // }
+
+// int main() {
+//     int num = resto(10, 3);
+//     printf("%d", num);
+
+//     return 0;
+// }
+
+// ############################# EJ 04 #######################################################################################
+
+void imprimirArr(int* arr, int cant) {
+    for (int x = 0; x < cant; x++) {
+        printf("%d, ", arr[x]);
+    } printf("\n");
 }
 
-// int resto(10, 3) {
-//     if (10>3) {
-//         return 1 + resto(7, 3);
-//     }
-// }
-
-// int resto(7, 3) {
-//     if (7>3) {
-//         return 1 + resto(4, 3);
-//     }
-// }
-
-// int resto(4, 3) {
-//     if (4>3) {
-//         return 1 + resto(1, 3);
-//     }
-// }
-
-// num = 3
-
-// int resto(1, 3) {
-//     if (1>3) {
-//         no
-//     } else {
-//         num = 1
-//     }
-// }
+void invertir(int* arr, int num) {
+    // printf("%d\n", *(arr));
+    if (num > 0) {
+        // printf("%d\n", *(arr+(num-1)));
+        // printf("%d\n", *(arr+1));
+        // printf("%d\n", *(arr+1+(num-2-1)));
+        int temp = *(arr+(num-1));
+        *(arr+(num-1)) = *(arr);
+        *(arr) = temp;
+        invertir(arr+1, num-2);
+    }
+}
 
 int main() {
-    int num = resto(10, 3);
-    printf("%d", num);
+    int* arr = NULL;
+    arr = malloc(5 * sizeof(int));
+    arr[0] = 6;
+    arr[1] = 2;
+    arr[2] = 7;
+    arr[3] = 1;
+    arr[4] = 8;
+
+    // printf("%d, ", *(arr+0));
+    imprimirArr(arr, 5);
+    invertir(arr, 5);
+    imprimirArr(arr, 5);
 
     return 0;
 }
