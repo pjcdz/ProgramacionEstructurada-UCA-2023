e28ca0cc3e1bc6e14078ad911e4d16b715db9f2f
Pablo Cardozo <cardozopabloj@gmail.com>
2023-05-24 19:38:03 -0300

    0502

--- 05.c
+++ 05.c
@@ -118,65 +118,115 @@
 
 // ############################# EJ 01.02 #######################################################################################
 
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+// struct employee {
+//     int id;
+//     char* name;
+//     struct address* datos;
+//     struct employee* sig;
+// };
+
+// typedef struct employee* e_node;
+
+// struct address {
+//     char* street;
+//     char* city;
+//     char* state;
+//     int zip;
+// } addr;
+
+// void append(e_node* ls, int id, char* name, char* street, char* city, char* state, int zip) {
+//     if ( *ls == NULL ) {
+//         *ls = malloc( sizeof(struct employee) );
+
+//         (*ls)->id = id;
+//         (*ls)->name = malloc( strlen(name) * sizeof(char) );
+//         strcpy ( (*ls)->name, name ); // ya se esta pasando con el * asi que no lo agrego nuevamente
+
+//         // (*ls)->datos = NULL; // Malloc reará automáticamente un puntero 
+//         // válido a la estructura address, por lo que no es necesario
+//         // inicializarlo con NULL antes de asignar memoria.
+
+//         (*ls)->datos = malloc ( sizeof(struct address) );
+//         (*ls)->datos->street =  malloc( strlen(street) * sizeof(char) );
+//         (*ls)->datos->city =  malloc( strlen(city) * sizeof(char) );
+//         (*ls)->datos->state =  malloc( strlen(state) * sizeof(char) );
+        
+//         strcpy ( (*ls)->datos->street, street ); 
+//         strcpy ( (*ls)->datos->city , city ); 
+//         strcpy ( (*ls)->datos->state, state );
+//         (*ls)->datos->zip = zip;
+        
+//         (*ls)->sig = NULL;
+//     } else {
+//         append( &((*ls)->sig), id, name, street, city, state, zip );
+//     }
+// }
+
+// void imprimirLsRecursiva (e_node ls) {
+//     if (ls != NULL) {
+//         printf("Datos del empleado: ID = %d, Nombre = %s\n", ls->id, ls->name);
+//         printf("Direccion: Calle = %s, State = %s, Ciudad = %s, ZIP = %d\n\n", ls->datos->street, ls->datos->state, ls->datos->state, ls->datos->zip);
+//         imprimirLsRecursiva (ls->sig);
+//     }
+// }
+
+// int main() {
+//     e_node ls = NULL;
+//     append( &ls, 1, "John Smith", "123 Main St.", "New York", "NY", 10001 );
+//     imprimirLsRecursiva(ls);
+
+//     return 0;
+// }
+
+// ############################# EJ 02 #######################################################################################
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-struct employee {
-    int id;
-    char* name;
-    struct address* datos;
-    struct employee* sig;
+struct s_nodo {
+    int val;
+    struct s_nodo* sig;
 };
 
-typedef struct employee* e_node;
+typedef struct s_nodo* t_nodo;
 
-struct address {
-    char* street;
-    char* city;
-    char* state;
-    int zip;
-} addr;
-
-void append(e_node* ls, int id, char* name, char* street, char* city, char* state, int zip) {
+void appendOrdenado (t_nodo* ls, int val) {
     if ( *ls == NULL ) {
-        *ls = malloc( sizeof(struct employee) );
-
-        (*ls)->id = id;
-        (*ls)->name = malloc( strlen(name) * sizeof(char) );
-        strcpy ( (*ls)->name, name ); // ya se esta pasando con el * asi que no lo agrego nuevamente
-
-        // (*ls)->datos = NULL; // Malloc reará automáticamente un puntero 
-        // válido a la estructura address, por lo que no es necesario
-        // inicializarlo con NULL antes de asignar memoria.
-        
-        (*ls)->datos = malloc ( sizeof(struct address) );
-        (*ls)->datos->street =  malloc( strlen(street) * sizeof(char) );
-        (*ls)->datos->city =  malloc( strlen(city) * sizeof(char) );
-        (*ls)->datos->state =  malloc( strlen(state) * sizeof(char) );
-        
-        strcpy ( (*ls)->datos->street, street ); 
-        strcpy ( (*ls)->datos->city , city ); 
-        strcpy ( (*ls)->datos->state, state );
-        (*ls)->datos->zip = zip;
-        
+        *ls = malloc(sizeof(struct s_nodo));
+        (*ls)->val = val;
         (*ls)->sig = NULL;
     } else {
-        append( &((*ls)->sig), id, name, street, city, state, zip );
+        if ( val < (*ls)->val ) {
+            int temp = (*ls)->val;
+            (*ls)->val = val;
+
+            appendOrdenado( &((*ls)->sig), temp );     
+        } else {
+            appendOrdenado( &((*ls)->sig), val ); 
+        }
+        
     }
 }
 
-void imprimirLsRecursiva (e_node ls) {
-    if (ls != NULL) {
-        printf("Datos del empleado: ID = %d, Nombre = %s\n", ls->id, ls->name);
-        printf("Direccion: Calle = %s, State = %s, Ciudad = %s, ZIP = %d\n\n", ls->datos->street, ls->datos->state, ls->datos->state, ls->datos->zip);
-        imprimirLsRecursiva (ls->sig);
+void imprimirLsRecursiva (t_nodo ls) {
+    if ( ls != NULL ) {
+        printf("val: %d, dirAct: <%p>, dirSig: <%p>\n", ls->val, ls, ls->sig);
+        imprimirLsRecursiva(ls->sig);
     }
 }
+
 
 int main() {
-    e_node ls = NULL;
-    append( &ls, 1, "John Smith", "123 Main St.", "New York", "NY", 10001 );
+    t_nodo ls = NULL;
+    appendOrdenado(&ls, 20);
+    appendOrdenado(&ls, 50);
+    appendOrdenado(&ls, 10);
+    appendOrdenado(&ls, 30);
+    
     imprimirLsRecursiva(ls);
 
     return 0;
