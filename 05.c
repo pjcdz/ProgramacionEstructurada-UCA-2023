0cd500b055e558e7bc63f5b40e7863a387db0376
Pablo Cardozo <cardozopabloj@gmail.com>
2023-05-23 22:16:50 -0300

    050102

--- 05.c
+++ 05.c
@@ -50,68 +50,134 @@
 
 // ############################# EJ 01.01 #######################################################################################
 
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+// struct s_nodo {
+//     char* nombre;
+//     char* apellido;
+//     int dni;
+//     struct s_nodo* sig;
+// };
+
+// typedef struct s_nodo* t_nodo;
+
+// void append(t_nodo* nodo, char* nombre, char* apellido, int dni) {
+//     if ( *nodo == NULL ) {
+//         *nodo = malloc(sizeof(struct s_nodo));
+//         // printf("%s, %s\n", nombre, apellido);
+//         (*nodo)->nombre = malloc( (strlen(nombre)) * sizeof(char) );
+//         strcpy( (*nodo)->nombre, nombre);
+//         (*nodo)->apellido = malloc( (strlen(apellido)) * sizeof(char) );
+//         strcpy( (*nodo)->apellido, apellido);
+//         (*nodo)->dni = dni;
+//         (*nodo)->sig = NULL;
+//     } else {
+//         append( &((*nodo)->sig), nombre, apellido, dni );
+//     }
+// }
+
+// void imprimirLsRecursiva (t_nodo ls) {
+//     if (ls != NULL) {
+//         printf("nombre: %s, apellido: %s, dni=%d, dirA=<%p>, dirSig=<%p>\n", ls->nombre, ls->apellido, ls->dni, ls, ls->sig);
+//         imprimirLsRecursiva(ls->sig);
+//     }
+// }
+
+// void imprimirLsRecursivaInvertida (t_nodo ls) {
+//     if (ls != NULL) {
+//         imprimirLsRecursivaInvertida(ls->sig);
+//         printf("nombre: %s, apellido: %s, dni=%d, dirA=<%p>, dirSig=<%p>\n", ls->nombre, ls->apellido, ls->dni, ls, ls->sig);
+//     }
+// }
+
+// void freeLista(t_nodo ls) {
+//     if (ls != NULL) {
+//         freeLista(ls->sig);
+//         free(ls->nombre);
+//         free(ls->apellido);
+//         free(ls);
+//     }
+// }
+
+// int main() {
+//     t_nodo ls = NULL;
+//     append(&ls, "Juan", "Martinez", 123);
+//     append(&ls, "Laura", "Adam", 456);
+//     append(&ls, "Andrew", "Ryan", 789);
+
+//     imprimirLsRecursiva(ls);
+//     printf("----------------\n");
+//     imprimirLsRecursivaInvertida(ls);
+
+//     freeLista(ls);
+
+//     return 0;
+// }
+
+// ############################# EJ 01.02 #######################################################################################
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-struct s_nodo {
-    char* nombre;
-    char* apellido;
-    int dni;
-    struct s_nodo* sig;
+struct employee {
+    int id;
+    char* name;
+    struct address* datos;
+    struct employee* sig;
 };
 
-typedef struct s_nodo* t_nodo;
+typedef struct employee* e_node;
 
-void append(t_nodo* nodo, char* nombre, char* apellido, int dni) {
-    if ( *nodo == NULL ) {
-        *nodo = malloc(sizeof(struct s_nodo));
-        // printf("%s, %s\n", nombre, apellido);
-        (*nodo)->nombre = malloc( (strlen(nombre)) * sizeof(char) );
-        strcpy( (*nodo)->nombre, nombre);
-        (*nodo)->apellido = malloc( (strlen(apellido)) * sizeof(char) );
-        strcpy( (*nodo)->apellido, apellido);
-        (*nodo)->dni = dni;
-        (*nodo)->sig = NULL;
+struct address {
+    char* street;
+    char* city;
+    char* state;
+    int zip;
+} addr;
+
+void append(e_node* ls, int id, char* name, char* street, char* city, char* state, int zip) {
+    if ( *ls == NULL ) {
+        *ls = malloc( sizeof(struct employee) );
+
+        (*ls)->id = id;
+        (*ls)->name = malloc( strlen(name) * sizeof(char) );
+        strcpy ( (*ls)->name, name ); // ya se esta pasando con el * asi que no lo agrego nuevamente
+
+        // (*ls)->datos = NULL; // Malloc reará automáticamente un puntero 
+        // válido a la estructura address, por lo que no es necesario
+        // inicializarlo con NULL antes de asignar memoria.
+        
+        (*ls)->datos = malloc ( sizeof(struct address) );
+        (*ls)->datos->street =  malloc( strlen(street) * sizeof(char) );
+        (*ls)->datos->city =  malloc( strlen(city) * sizeof(char) );
+        (*ls)->datos->state =  malloc( strlen(state) * sizeof(char) );
+        
+        strcpy ( (*ls)->datos->street, street ); 
+        strcpy ( (*ls)->datos->city , city ); 
+        strcpy ( (*ls)->datos->state, state );
+        (*ls)->datos->zip = zip;
+        
+        (*ls)->sig = NULL;
     } else {
-        append( &((*nodo)->sig), nombre, apellido, dni );
+        append( &((*ls)->sig), id, name, street, city, state, zip );
     }
 }
 
-void imprimirLsRecursiva (t_nodo ls) {
+void imprimirLsRecursiva (e_node ls) {
     if (ls != NULL) {
-        printf("nombre: %s, apellido: %s, dni=%d, dirA=<%p>, dirSig=<%p>\n", ls->nombre, ls->apellido, ls->dni, ls, ls->sig);
-        imprimirLsRecursiva(ls->sig);
-    }
-}
-
-void imprimirLsRecursivaInvertida (t_nodo ls) {
-    if (ls != NULL) {
-        imprimirLsRecursivaInvertida(ls->sig);
-        printf("nombre: %s, apellido: %s, dni=%d, dirA=<%p>, dirSig=<%p>\n", ls->nombre, ls->apellido, ls->dni, ls, ls->sig);
-    }
-}
-
-void freeLista(t_nodo ls) {
-    if (ls != NULL) {
-        freeLista(ls->sig);
-        free(ls->nombre);
-        free(ls->apellido);
-        free(ls);
+        printf("Datos del empleado: ID = %d, Nombre = %s\n", ls->id, ls->name);
+        printf("Direccion: Calle = %s, State = %s, Ciudad = %s, ZIP = %d\n\n", ls->datos->street, ls->datos->state, ls->datos->state, ls->datos->zip);
+        imprimirLsRecursiva (ls->sig);
     }
 }
 
 int main() {
-    t_nodo ls = NULL;
-    append(&ls, "Juan", "Martinez", 123);
-    append(&ls, "Laura", "Adam", 456);
-    append(&ls, "Andrew", "Ryan", 789);
-
+    e_node ls = NULL;
+    append( &ls, 1, "John Smith", "123 Main St.", "New York", "NY", 10001 );
     imprimirLsRecursiva(ls);
-    printf("----------------\n");
-    imprimirLsRecursivaInvertida(ls);
-
-    freeLista(ls);
 
     return 0;
 }
